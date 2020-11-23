c07d33de0386c31cb8ea244942e446dd95fb67fc
VahidGhadiri <whydrf@gmail.com>
2020-11-23 17:27:21 +0330

    Dynamic routing has been added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
-import "./App.css"
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path={"/"} component={HomePage} />
+        <Route exact={true} path={"/Hats"} component={HatsPage} />
+      </Switch>
     </div>
   );
 }
